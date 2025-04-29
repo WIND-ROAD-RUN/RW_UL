a29a17753953aa00c70865d335d4ead18ca6df82
rw <3461571786@qq.com>
2025-04-29 16:45:52 +0800

    简化检测框绘制逻辑
    在 `imet_ModelEngine.cpp` 中，删除了 `CLASS_NAMES` 和 `COLORS` 的定义。将颜色使用改为固定值 `{ 0, 114, 189 }`，并修改类名显示方式为直接使用 `class_id`。这些更改旨在简化代码。

--- ime/imet/src/imet_ModelEngine.cpp
+++ ime/imet/src/imet_ModelEngine.cpp
@@ -7,36 +7,6 @@
 
 namespace rw {
 	namespace imet {
-		const std::vector<std::string> CLASS_NAMES = {
-	"person",         "bicycle",    "car",           "motorcycle",    "airplane",     "bus",           "train",
-	"truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",    "parking meter", "bench",
-	"bird",           "cat",        "dog",           "horse",         "sheep",        "cow",           "elephant",
-	"bear",           "zebra",      "giraffe",       "backpack",      "umbrella",     "handbag",       "tie",
-	"suitcase",       "frisbee",    "skis",          "snowboard",     "sports ball",  "kite",          "baseball bat",
-	"baseball glove", "skateboard", "surfboard",     "tennis racket", "bottle",       "wine glass",    "cup",
-	"fork",           "knife",      "spoon",         "bowl",          "banana",       "apple",         "sandwich",
-	"orange",         "broccoli",   "carrot",        "hot dog",       "pizza",        "donut",         "cake",
-	"chair",          "couch",      "potted plant",  "bed",           "dining table", "toilet",        "tv",
-	"laptop",         "mouse",      "remote",        "keyboard",      "cell phone",   "microwave",     "oven",
-	"toaster",        "sink",       "refrigerator",  "book",          "clock",        "vase",          "scissors",
-	"teddy bear",     "hair drier", "toothbrush" };
-
-		const std::vector<std::vector<unsigned int>> COLORS = {
-			{0, 114, 189},   {217, 83, 25},   {237, 177, 32},  {126, 47, 142},  {119, 172, 48},  {77, 190, 238},
-			{162, 20, 47},   {76, 76, 76},    {153, 153, 153}, {255, 0, 0},     {255, 128, 0},   {191, 191, 0},
-			{0, 255, 0},     {0, 0, 255},     {170, 0, 255},   {85, 85, 0},     {85, 170, 0},    {85, 255, 0},
-			{170, 85, 0},    {170, 170, 0},   {170, 255, 0},   {255, 85, 0},    {255, 170, 0},   {255, 255, 0},
-			{0, 85, 128},    {0, 170, 128},   {0, 255, 128},   {85, 0, 128},    {85, 85, 128},   {85, 170, 128},
-			{85, 255, 128},  {170, 0, 128},   {170, 85, 128},  {170, 170, 128}, {170, 255, 128}, {255, 0, 128},
-			{255, 85, 128},  {255, 170, 128}, {255, 255, 128}, {0, 85, 255},    {0, 170, 255},   {0, 255, 255},
-			{85, 0, 255},    {85, 85, 255},   {85, 170, 255},  {85, 255, 255},  {170, 0, 255},   {170, 85, 255},
-			{170, 170, 255}, {170, 255, 255}, {255, 0, 255},   {255, 85, 255},  {255, 170, 255}, {85, 0, 0},
-			{128, 0, 0},     {170, 0, 0},     {212, 0, 0},     {255, 0, 0},     {0, 43, 0},      {0, 85, 0},
-			{0, 128, 0},     {0, 170, 0},     {0, 212, 0},     {0, 255, 0},     {0, 0, 43},      {0, 0, 85},
-			{0, 0, 128},     {0, 0, 170},     {0, 0, 212},     {0, 0, 255},     {0, 0, 0},       {36, 36, 36},
-			{73, 73, 73},    {109, 109, 109}, {146, 146, 146}, {182, 182, 182}, {219, 219, 219}, {0, 114, 189},
-			{80, 183, 189},  {128, 128, 0} };
-
 		ModelEngine_yolov11_obb::ModelEngine_yolov11_obb(std::string model_path, nvinfer1::ILogger& logger)
 		{
 			init(model_path, logger);
@@ -124,17 +94,16 @@ namespace rw {
 				auto box = detection.bbox;
 				auto class_id = detection.class_id;
 				auto conf = detection.conf;
-				cv::Scalar color = cv::Scalar(COLORS[class_id][0], COLORS[class_id][1], COLORS[class_id][2]);
 				box.x = box.x / ratio_w;
 				box.y = box.y / ratio_h;
 				box.width = box.width / ratio_w;
 				box.height = box.height / ratio_h;
-				rectangle(image, cv::Point(box.x, box.y), cv::Point(box.x + box.width, box.y + box.height), color, 3);
+				rectangle(image, cv::Point(box.x, box.y), cv::Point(box.x + box.width, box.y + box.height), { 0, 114, 189 }, 3);
 				// Detection box text
-				std::string class_string = CLASS_NAMES[class_id] + ' ' + std::to_string(conf).substr(0, 4);
+				std::string class_string = std::to_string(class_id) + ' ' + std::to_string(conf).substr(0, 4);
 				cv::Size text_size = cv::getTextSize(class_string, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
 				cv::Rect text_rect(box.x, box.y - 40, text_size.width + 10, text_size.height + 20);
-				rectangle(image, text_rect, color, cv::FILLED);
+				rectangle(image, text_rect, { 0, 114, 189 }, cv::FILLED);
 				putText(image, class_string, cv::Point(box.x + 5, box.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
 			}
 		}
