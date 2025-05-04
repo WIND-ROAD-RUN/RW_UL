2169b5ccd56eaeae8455f1d788f551ce3702905d
RoadWind <3461571786@qq.com>
2025-05-04 14:04:38 +0800

    添加 ModelEngine_yolov11_obb 类的初始实现
    在 `imeo_ModelEngine_yolov11_obb.hpp` 文件中，添加了头文件保护 `#pragma once`，引入了 OpenCV 和字符串库，并定义了命名空间 `rw` 和 `imeo`，其中包含一个空的类 `ModelEngine_yolov11_obb`。
    
    在 `imeo_ModelEngine_yolov11_obb.cpp` 文件中，添加了对头文件的引用，并在命名空间中实现了空的内容。

---
+++ ime/imeo/include/imeo_ModelEngine_yolov11_obb.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include"opencv2/opencv.hpp"
+
+#include<string>
+
+namespace rw {
+    namespace imeo {
+        class ModelEngine_yolov11_obb {
+        
+        };
+    }
+}
---
+++ ime/imeo/src/imeo_ModelEngine_yolov11_obb.cpp
@@ -0,0 +1,9 @@
+#include"imeo_ModelEngine_yolov11_obb.hpp"
+
+namespace rw
+{
+	namespace imeo
+	{
+		
+	}
+}
