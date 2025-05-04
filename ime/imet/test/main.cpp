b365365d644873bb36da8a70c6adf4346c65f594
RoadWind <3461571786@qq.com>
2025-05-04 11:19:31 +0800

    添加imeo新项

--- ime/imet/test/main.cpp
+++ ime/imet/test/main.cpp
@@ -15,45 +15,17 @@ class Logger : public nvinfer1::ILogger {
 	}
 }logger;
 
-int main() {
-	rw::imet::ModelEngine_yolov11_seg model_engine(R"(C:\Users\zfkj\Desktop\model\best_seg.engine)", logger);
-
-	const string path{ R"(C:\Users\zfkj\Desktop\1\images\train\05113212738.jpg)" };
-
-	Mat image = imread(path);
-	if (image.empty())
-	{
-		cerr << "error reading image: " << path << endl;
-	}
-	vector<DetectionSeg> objects;
-	auto start = std::chrono::system_clock::now();
-	model_engine.preprocess(image);
-	model_engine.infer();
-	model_engine.postprocess(objects);
-	model_engine.draw(image, objects);
-	auto end = std::chrono::system_clock::now();
-
-	auto tc = (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
-	printf("cost %2.4lf ms\n", tc);
-
-	imshow("result", image);
-
-	cv::waitKey(0);
-	return 0;
-
-}
-
 //int main() {
-//	rw::imet::ModelEngine_yolov11_obb model_engine(R"(C:\Users\zfkj\Desktop\model\best.engine)", logger);
+//	rw::imet::ModelEngine_yolov11_seg model_engine(R"(C:\Users\zfkj\Desktop\model\best_seg.engine)", logger);
 //
-//	const string path{ R"(C:\Users\zfkj\Desktop\20250411poyan\images\100828324793.jpg)"};
+//	const string path{ R"(C:\Users\zfkj\Desktop\1\images\train\05113212738.jpg)" };
 //
 //	Mat image = imread(path);
 //	if (image.empty())
 //	{
-//		cerr << "Error reading image: " << path << endl;
+//		cerr << "error reading image: " << path << endl;
 //	}
-//	vector<Detection> objects;
+//	vector<DetectionSeg> objects;
 //	auto start = std::chrono::system_clock::now();
 //	model_engine.preprocess(image);
 //	model_engine.infer();
@@ -64,9 +36,37 @@ int main() {
 //	auto tc = (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
 //	printf("cost %2.4lf ms\n", tc);
 //
-//	imshow("Result", image);
+//	imshow("result", image);
 //
-//	waitKey(0);
+//	cv::waitKey(0);
 //	return 0;
 //
 //}
+
+int main() {
+	rw::imet::ModelEngine_yolov11_obb model_engine(R"(C:\Users\rw\Desktop\model\best.engine)", logger);
+
+	const string path{ R"(C:\Users\rw\Desktop\1.png)"};
+
+	Mat image = imread(path);
+	if (image.empty())
+	{
+		cerr << "Error reading image: " << path << endl;
+	}
+	vector<Detection> objects;
+	auto start = std::chrono::system_clock::now();
+	model_engine.preprocess(image);
+	model_engine.infer();
+	model_engine.postprocess(objects);
+	model_engine.draw(image, objects);
+	auto end = std::chrono::system_clock::now();
+
+	auto tc = (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
+	printf("cost %2.4lf ms\n", tc);
+
+	imshow("Result", image);
+
+	waitKey(0);
+	return 0;
+
+}
