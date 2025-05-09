fd8470356e2128a7b8f44a4cf869b9d7fcbbb726
rw <3461571786@qq.com>
2025-05-09 09:35:56 +0800

    添加onnxruntime项目

---
+++ Project/yolo/src/YOLOv11.h
@@ -0,0 +1,57 @@
+#pragma once
+#include<onnxruntime_cxx_api.h>
+#include "NvInfer.h"
+#include <opencv2/opencv.hpp>
+
+using namespace nvinfer1;
+using namespace std;
+using namespace cv;
+
+struct Detection
+{
+    float conf;
+    int class_id;
+    Rect bbox;
+};
+
+class YOLOv11
+{
+
+public:
+    bool trt = 0;
+    Ort::Env env;
+    Ort::Session session= Ort::Session(nullptr);
+    std::vector<Ort::Value>output_tensors;
+    std::vector<const char*> input_node_names;
+    std::vector<const char*> output_node_names;
+    Ort::Value input_tensor = Ort::Value(nullptr);
+    std::vector<Ort::Value> ort_inputs;
+    cv::Mat infer_image;
+    YOLOv11(string model_path, nvinfer1::ILogger& logger,bool trt);
+    ~YOLOv11();
+
+    void preprocess(Mat& image);
+    void infer();
+    void postprocess(vector<Detection>& output);
+    void draw(Mat& image, const vector<Detection>& output);
+
+private:
+    void init(std::string engine_path, nvinfer1::ILogger& logger);
+
+    float* gpu_buffers[2];               //!< The vector of device buffers needed for engine execution
+    float* cpu_output_buffer;
+    IRuntime* runtime;                 //!< The TensorRT runtime used to deserialize the engine
+    ICudaEngine* engine;               //!< The TensorRT engine used to run the network
+    IExecutionContext* context;        //!< The context for executing inference using an ICudaEngine
+
+    // Model parameters
+    int input_w;
+    int input_h;
+    int num_detections;
+    int detection_attribute_size;
+    int num_classes = 80;
+    float conf_threshold = 0.3f;
+    float nms_threshold = 0.4f;
+
+    vector<Scalar> colors;
+};
