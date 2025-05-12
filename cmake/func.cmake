6b7f0f3336b469eabc53644d47daf6067703bbcb
zfkj <34161571786@qq.com>
2025-05-12 18:18:24 +0800

    生成目标时拷贝tensort的转换脚本

--- cmake/func.cmake
+++ cmake/func.cmake
@@ -4,6 +4,12 @@ function(find_all_libs dir output_var)
     set(${output_var} ${libs} PARENT_SCOPE)
 endfunction()
 
+# 查找所有 .exe 文件
+function(find_all_exes dir output_var)
+    file(GLOB_RECURSE libs "${dir}/*.exe")
+    set(${output_var} ${libs} PARENT_SCOPE)
+endfunction()
+
 # 查找所有 .dll 文件
 function(find_all_dlls dir output_var)
     file(GLOB_RECURSE dlls "${dir}/*.dll")
