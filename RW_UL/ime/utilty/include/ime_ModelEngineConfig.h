53eee2f28ebda0a91c7da11cd9a952c8fb2e1291
RoadWind <3461571786@qq.com>
2025-05-17 09:35:11 +0800

    重构代码

--- RW_UL/ime/utilty/include/ime_ModelEngineConfig.h
+++ RW_UL/ime/utilty/include/ime_ModelEngineConfig.h
@@ -14,12 +14,13 @@ namespace rw {
 
 	struct ModelEngineConfig {
 	public:
-		//控制检测结果的最低置信度，过滤低置信度的候选框。
+		//Confidence threshold to filter out low-confidence candidates.
 		float conf_threshold = 0.3f;
-		//控制非极大值抑制的 IoU 阈值，移除重叠的候选框。
+		//Control the maximum number of candidates to be retained.
 		float nms_threshold = 0.4f;
 	public:
-		std::vector<size_t> need_keep_classids{};
+		//The set of classid will be together for nms
+		std::vector<size_t> classids_nms_together{};
 	public:
 		std::string modelPath;
 	public:
