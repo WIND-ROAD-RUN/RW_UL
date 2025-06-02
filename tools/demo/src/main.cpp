c56be0be5d4662a962b0e0faae90472c02ae204a
RoadWind <3461571786@qq.com>
2025-06-02 16:39:32 +0800

    修改保存格式为jpg

--- tools/demo/src/main.cpp
+++ tools/demo/src/main.cpp
@@ -2,6 +2,7 @@
 #include <QtWidgets/QApplication>
 #include"demoConfig.hpp"
 #include"ThumbnailsViewer.hpp"
+#include"opencv2/opencv.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -15,5 +16,6 @@ int main(int argc, char* argv[])
 	ThumbnailsViewer viewer;
 	viewer.setRootPath(R"(C:\Users\rw\Desktop\1)");
 	viewer.show();
+
 	return a.exec();
 }
