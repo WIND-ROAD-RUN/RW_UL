bd85229a62d4156f097645333839f141afcf2a28
RoadWind <3461571786@qq.com>
2025-06-11 16:51:29 +0800

    重构 ActivationConfig 结构体并添加测试用例
    在 `cla_ActivationConfig.hpp` 中，将 `ActivationConfig` 结构体的访问权限调整为 `private`，并添加构造函数、析构函数、赋值运算符和比较运算符的重载。增加了 `insert`、`erase`、`serialize` 和 `deserialize` 方法。
    
    在 `cla_ActivationConfig.cpp` 中，实现了构造函数、运算符重载及序列化相关方法。
    
    在 `cla_ActivationConfig_t.hpp` 中，添加头文件保护和 Google Test 的包含。
    
    在 `cla_ActivationConfig_t.cpp` 中，增加多个测试用例，验证 `ActivationConfig` 的功能和正确性。

--- RW_UL/cla/include/ActivationCodeModule/cla_ActivationConfig.hpp
+++ RW_UL/cla/include/ActivationCodeModule/cla_ActivationConfig.hpp
@@ -12,8 +12,27 @@ namespace rw
 
 		struct ActivationConfig
 		{
-		public:
+		private:
 			std::map<ConfigName, ConfigValue> configs;
+		public:
+			ActivationConfig() = default;
+			explicit ActivationConfig(const std::string& serialized);
+			operator std::string() const;
+
+			~ActivationConfig() = default;
+			ActivationConfig(const ActivationConfig& obj);
+			ActivationConfig& operator=(const ActivationConfig& obj);
+			bool operator==(const ActivationConfig& obj) const;
+			bool operator!=(const ActivationConfig& obj) const;
+		public:
+			void insert(const ConfigName & name,const ConfigName &value);
+			void erase(const ConfigName& name, const ConfigName& value);
+		private:
+			std::string serialize() const;
+		public:
+			static std::string serialize(const ActivationConfig& config);
+			static ActivationConfig deserialize(const std::string& data);
+
 		};
 	}
 }
