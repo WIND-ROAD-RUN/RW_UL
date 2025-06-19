213e2320fbd9ab070cd273a6f454b5aedd5969f0
zzw <zzw@1409256110>
2025-06-19 16:24:11 +0800

    添加dsl_TimeBasedCache_t测试

--- RW_UL/dsl/test/src/dsl_TimeBasedCache_t.cpp
+++ RW_UL/dsl/test/src/dsl_TimeBasedCache_t.cpp
@@ -4,7 +4,7 @@
 namespace dsl_TimeBasedCache
 {
 
-	TEST(TimeDouble,insert)
+	TEST(TimeDouble, insert)
 	{
 		rw::dsl::TimeBasedCache<double, double> cache(50);
 		cache.insert(123, 123);
@@ -29,13 +29,13 @@ namespace dsl_TimeBasedCache
 		cache.insert(127, 127);
 		cache.insert(128, 1278);
 
-		auto result=cache.query(125, 2, true, true);
-		std::vector<double> standard{123,124};
+		auto result = cache.query(125, 2, true, true);
+		std::vector<double> standard{ 123,124 };
 		ASSERT_EQ(result, standard);
 
 
 		auto result1 = cache.query(125, 2, false, true);
-		standard={ 126,127 };
+		standard = { 126,127 };
 		ASSERT_EQ(result1, standard);
 
 		auto result2 = cache.query(125, 2, false, false);
@@ -47,4 +47,113 @@ namespace dsl_TimeBasedCache
 		ASSERT_EQ(result3, standard);
 	}
 
+	TEST(TimeDouble, queryWithTime)
+	{
+		rw::dsl::TimeBasedCache<double, double> cache(50);
+		cache.insert(120, 120);
+		cache.insert(121, 121);
+		cache.insert(122, 122);
+		cache.insert(123, 123);
+		cache.insert(124, 124);
+		cache.insert(125, 125);
+		cache.insert(126, 126);
+		cache.insert(127, 127);
+		cache.insert(128, 128);
+
+		auto result = cache.queryWithTime(125, 2, true, true);
+		std::vector<double> standard = { 124,125 };
+		ASSERT_EQ(result, standard);
+
+		auto result1 = cache.queryWithTime(125, 2, true, false);
+		standard = { 125,124 };
+		ASSERT_EQ(result1, standard);
+
+		auto result2 = cache.queryWithTime(125, 2, false, true);
+		standard = { 125,126 };
+		ASSERT_EQ(result2, standard);
+
+		auto result3 = cache.queryWithTime(125, 2, false, false);
+		standard = { 126,125 };
+		ASSERT_EQ(result3, standard);
+	}
+
+	TEST(TimeDouble, queryToMap)
+	{
+		rw::dsl::TimeBasedCache<double, double> cache(50);
+		cache.insert(120, 120);
+		cache.insert(121, 121);
+		cache.insert(122, 122);
+		cache.insert(123, 123);
+		cache.insert(124, 124);
+		cache.insert(125, 125);
+		cache.insert(126, 126);
+		cache.insert(127, 127);
+		cache.insert(128, 128);
+
+		auto result = cache.queryToMap(125, 2, true, true);
+		std::unordered_map<double,double> standard = {{ 123,123},{124,124} };
+		ASSERT_EQ(result, standard);
+
+		auto result1 = cache.queryToMap(125, 2, true, false);
+		standard = { {124,124},{123,123}};
+		ASSERT_EQ(result1, standard);
+
+		auto result2 = cache.queryToMap(125, 2, false, true);
+		standard = { {126,126},{127,127} };
+		ASSERT_EQ(result2, standard);
+
+		auto result3 = cache.queryToMap(125, 2, false, false);
+		standard = { {127,127},{126,126}};
+		ASSERT_EQ(result3, standard);
+	}
+
+	TEST(TimeDouble, queryWithTimeToMap)
+	{
+		rw::dsl::TimeBasedCache<double, double> cache(50);
+		cache.insert(120, 120);
+		cache.insert(121, 121);
+		cache.insert(122, 122);
+		cache.insert(123, 123);
+		cache.insert(124, 124);
+		cache.insert(125, 125);
+		cache.insert(126, 126);
+		cache.insert(127, 127);
+		cache.insert(128, 128);
+
+		auto result = cache.queryWithTimeToMap(125, 2, true, true);
+		std::unordered_map<double, double> standard = { { 124,124},{125,125} };
+		ASSERT_EQ(result, standard);
+
+		auto result1 = cache.queryWithTimeToMap(125, 2, true, false);
+		standard = { {125,125},{124,124} };
+		ASSERT_EQ(result1, standard);
+
+		auto result2 = cache.queryWithTimeToMap(125, 2, false, true);
+		standard = { {125,125},{126,126} };
+		ASSERT_EQ(result2, standard);
+
+		auto result3 = cache.queryWithTimeToMap(125, 2, false, false);
+		standard = { {126,126},{125,125} };
+		ASSERT_EQ(result3, standard);
+	}
+
+	TEST(TimeDouble, size)
+	{
+		rw::dsl::TimeBasedCache<double, double> cache(50);
+		cache.insert(120, 120);
+		cache.insert(121, 121);
+		cache.insert(122, 122);
+		cache.insert(123, 123);
+		cache.insert(124, 124);
+		cache.insert(125, 125);
+		cache.insert(126, 126);
+		cache.insert(127, 127);
+		cache.insert(128, 128);
+
+		auto result = cache.query(125, 2, true, true);
+		ASSERT_EQ(result.size(), 2);
+
+		result = cache.queryWithTime(125, 2, true, true);
+		ASSERT_EQ(result.size(), 2);
+	}
 }
