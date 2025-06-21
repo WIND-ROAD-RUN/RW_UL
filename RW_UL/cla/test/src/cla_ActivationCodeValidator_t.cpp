7b6aaa75e792d017720cdee482dc80c578139b14
RoadWind <3461571786@qq.com>
2025-06-21 16:14:12 +0800

    补充validateActivationCode测试

--- RW_UL/cla/test/src/cla_ActivationCodeValidator_t.cpp
+++ RW_UL/cla/test/src/cla_ActivationCodeValidator_t.cpp
@@ -1,10 +1,16 @@
 #include"cla_ActivationCodeValidator_t.hpp"
 
+#include "ActivationCodeModule/cla_ActivationCodeGenerator.hpp"
+
 namespace cla_ActivationCodeModule
 {
-	TEST(ActivationCodeValidator)
+	TEST(ActivationCodeValidator, validateActivationCode)
 	{
-		
+		rw::cla::ActivationCodeGenerator generator;
+		auto code=generator.generateActivationCode("temp");
+		rw::cla::ActivationCodeValidator validator;
+		auto result=validator.validateActivationCode(code, "temp");
+		ASSERT_EQ(result, true);
 	}
 }
 
