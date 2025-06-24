56a306b21e28b293b4bd8282713f738b789b4d0d
RoadWind <3461571786@qq.com>
2025-06-24 16:29:48 +0800

    添加加密配置

--- RW_UL/cla/include/EncrpyModule/cla_EncrpyUtilty.hpp
+++ RW_UL/cla/include/EncrpyModule/cla_EncrpyUtilty.hpp
@@ -1,16 +1,24 @@
 #pragma once
 
+#include <string>
+
 namespace rw {
 	namespace cla {
 		struct EncryptConfig
 		{
-			
+		public:
+			std::string encryptString{};
+		public: 
+			std::string key{};
 		};
 
 
 		struct DecryptConfig
 		{
-			
+		public:
+			std::string decryptString{ };
+		public:
+			std::string key{};
 		};
 
 
