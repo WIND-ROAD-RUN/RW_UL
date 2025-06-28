2e98e178f8200cf77560157bb1c23123bd2553ba
zfkj <34161571786@qq.com>
2025-06-28 13:43:17 +0800

    添加配置文件的检查逻辑

--- Project/ButtonScanner/source/src/func/RunEnvCheck.cpp
+++ Project/ButtonScanner/source/src/func/RunEnvCheck.cpp
@@ -1,10 +1,17 @@
 #include"RunEnvCheck.hpp"
 
+#include <QFile>
 #include <QMessageBox>
 
 #include "ButtonScannerDlgExposureTimeSet.hpp"
+#include "ButtonScannerDlgHideScoreSet.hpp"
+#include "ButtonScannerDlgProductSet.hpp"
+#include "ButtonScannerMainWindow.hpp"
+#include "ButtonScannerProduceLineSet.hpp"
 #include"oso_StorageContext.hpp"
 #include"ButtonUtilty.h"
+#include "WarningIOSetConfig.hpp"
+#include"cdm_ButtonScannerDlgWarningManager.h"
 
 bool RunEnvCheck::isSingleInstance(const QString& instanceName)
 {
@@ -27,7 +34,96 @@ bool RunEnvCheck::isProcessRunning(const QString& processName)
 bool RunEnvCheck::checkConfigIsOk()
 {
 	rw::oso::StorageContext storageContext(rw::oso::StorageType::Xml);
-	cdm::ButtonScannerDlgExposureTimeSet config = *storageContext.load(globalPath.configRootPath.toStdString());
+	try
+	{
+		if (QFile::exists(globalPath.exposureTimeSetConfigPath)) {
+			cdm::ButtonScannerDlgExposureTimeSet config = *storageContext.load(globalPath.exposureTimeSetConfigPath.toStdString());
+		}
+	}
+	catch (std::runtime_error & e)
+	{
+		QMessageBox::warning(nullptr, "提示", "ButtonScannerDlgExposureTimeSet配置已失效，请联系维护人员。");
+		return false;
+
+	}
+
+	try
+	{
+		if (QFile::exists(globalPath.dlgHideScoreSetPath)) {
+			cdm::DlgHideScoreSet config = *storageContext.load(globalPath.dlgHideScoreSetPath.toStdString());
+		}
+	}
+	catch (std::runtime_error& e)
+	{
+		QMessageBox::warning(nullptr, "提示", "DlgHideScoreSet配置已失效，请联系维护人员。");
+		return false;
+
+	}
+
+	try
+	{
+		if (QFile::exists(globalPath.dlgProduceLineSetConfigPath)) {
+			cdm::ButtonScannerProduceLineSet config = *storageContext.load(globalPath.dlgProduceLineSetConfigPath.toStdString());
+		}
+	}
+	catch (std::runtime_error& e)
+	{
+		QMessageBox::warning(nullptr, "提示", "ButtonScannerProduceLineSet配置已失效，请联系维护人员。");
+		return false;
+
+	}
+
+	try
+	{
+		if (QFile::exists(globalPath.dlgProdutSetConfigPath)) {
+			cdm::ButtonScannerDlgProductSet config = *storageContext.load(globalPath.dlgProdutSetConfigPath.toStdString());
+		}
+	}
+	catch (std::runtime_error& e)
+	{
+		QMessageBox::warning(nullptr, "提示", "ButtonScannerDlgProductSet配置已失效，请联系维护人员。");
+		return false;
+
+	}
+
+	try
+	{
+		if (QFile::exists(globalPath.mainWindowConfigPath)) {
+			cdm::ButtonScannerMainWindow config = *storageContext.load(globalPath.mainWindowConfigPath.toStdString());
+		}
+	}
+	catch (std::runtime_error& e)
+	{
+		QMessageBox::warning(nullptr, "提示", "ButtonScannerMainWindow配置已失效，请联系维护人员。");
+		return false;
+
+	}
+
+	try
+	{
+		if (QFile::exists(globalPath.warningIOSetConfigPath)) {
+			cdm::WarningIOSetConfig config = *storageContext.load(globalPath.warningIOSetConfigPath.toStdString());
+		}
+	}
+	catch (std::runtime_error& e)
+	{
+		QMessageBox::warning(nullptr, "提示", "WarningIOSetConfig配置已失效，请联系维护人员。");
+		return false;
+	}
+
+	try
+	{
+		if (QFile::exists(globalPath.warningManagerConfigPath)) {
+			rw::cdm::ButtonScannerDlgWarningManager config = *storageContext.load(globalPath.warningManagerConfigPath.toStdString());
+		}
+	}
+	catch (std::runtime_error& e)
+	{
+		QMessageBox::warning(nullptr, "提示", "ButtonScannerDlgWarningManager配置已失效，请联系维护人员。");
+		return false;
+	}
+
+
 
 	return true;
 }
@@ -51,6 +147,11 @@ EnvCheckResult RunEnvCheck::envCheck()
 	if (!isSingleInstance(instanceName)) {
 		QMessageBox::critical(nullptr, "错误", "程序已在运行中，无法启动多个实例。");
 		return EnvCheckResult::EnvError; // 退出程序
+	}
+
+	if (!checkConfigIsOk())
+	{
+		return EnvCheckResult::EnvError; 
 	}
 
 	return EnvCheckResult::EnvOk;
