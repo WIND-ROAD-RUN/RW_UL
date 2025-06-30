8cbea087774abaf897a6f3de6808ab25b9e30e7d
RoadWind <3461571786@qq.com>
2025-06-30 09:46:05 +0800

    分离ButtonScanner

--- Project/ButtonScanner/source/bat/ConvertOnnxToEngineColor1.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM If you need to activate a conda environment, uncomment and modify the next line
-REM call conda activate yolov11
-
-REM Run trtexec to convert ONNX to TensorRT engine
-.\trtexec.exe --onnx=.\runs\detect\train\weights\best.onnx --saveEngine=D:\zfkjData\ButtonScanner\ModelStorage\Temp\customOO1.engine
