3c12379f38355ea180affb5caf7eae5c48bd13ac
RoadWind <3461571786@qq.com>
2025-06-30 10:15:04 +0800

    提炼SmartCroppingOfBags

--- Project/SmartCroppingOfBags/src/data/Utilty.cpp
+++
@@ -1,8 +0,0 @@
-#include "Utilty.hpp"
-
-
-size_t ControlLines::qiedaoIn = 0;
-size_t ControlLines::chuiqiOut = 0;
-size_t ControlLines::baojinghongdengOUT = 0;
-size_t ControlLines::yadaiOut = 0;
-size_t ControlLines::tifeiOut = 0;
--- Project/SmartCroppingOfBags/src/func/ImageProcessorUtilty.cpp
+++
@@ -1,1 +0,0 @@
-#include"ImageProcessorUtilty.h"
