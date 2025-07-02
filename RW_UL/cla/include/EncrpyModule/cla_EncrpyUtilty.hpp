7008ef571b5ca010025e1e766d5775a5a8129238
RoadWind <3461571786@qq.com>
2025-07-02 16:17:53 +0800

    完成SymmetricEncryptor模块以及测试

--- RW_UL/cla/include/EncrpyModule/cla_EncrpyUtilty.hpp
+++ RW_UL/cla/include/EncrpyModule/cla_EncrpyUtilty.hpp
@@ -8,11 +8,21 @@ namespace rw {
 		struct SymmetricEncryptorContext
 		{
 		public:
-			ActivationCodeStruct decryptString{ };
+			ActivationBits bits{};
 		public:
-			ActivationCodeStruct encryptString{};
+			std::string encryptString{ };
 		public:
+			std::string sourceString{};
+		public:
+			//注意这里的密钥最好是32位的字符串，AES-256需要32字节的密钥
 			std::string key{};
+
+		};
+
+		class KeyGenerator {
+		public:
+			// Function to map any string to a 32-bit key
+			static std::string generate32BitKey(const std::string& input);
 		};
 	}
 }
