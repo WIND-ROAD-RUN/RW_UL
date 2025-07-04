c35892317743aa00c682665b29ed9294ca8f3f8c
RoadWind <3461571786@qq.com>
2025-07-04 16:05:28 +0800

    修改demo逻辑

--- tools/demo/src/main.cpp
+++ tools/demo/src/main.cpp
@@ -13,23 +13,26 @@ int main(int argc, char* argv[])
 	QApplication a(argc, argv);
 
 	// 创建一个 640x640 的黄色 QImage
-	QImage image(640, 640, QImage::Format_RGB32);
-	image.fill(Qt::yellow); // 填充为黄色
+	//QImage image(640, 640, QImage::Format_RGB32);
+	//image.fill(Qt::yellow); // 填充为黄色
 
-	rw::rqw::ImagePainter::PainterConfig config;
-	config.text = "sadawdawdadwd";
-	config.textLocate = rw::rqw::ImagePainter::PainterConfig::TextLocate::RightBottomOut;
-	rw::DetectionRectangleInfo det;
-	det.leftBottom = { 100, 500 };
-	det.leftTop = { 100, 100 };
-	det.rightBottom = { 500, 500 };
-	det.rightTop = { 500, 100 };
-	det.center_x = 150;
-	det.center_y = 150;
-	rw::rqw::ImagePainter::drawShapesOnSourceImg(image, det, config);
-	QLabel label;
-	label.setPixmap(QPixmap::fromImage(image));
-	label.show();
+	//rw::rqw::ImagePainter::PainterConfig config;
+	//config.text = "sadawdawdadwd";
+	//config.textLocate = rw::rqw::ImagePainter::PainterConfig::TextLocate::RightBottomOut;
+	//rw::DetectionRectangleInfo det;
+	//det.leftBottom = { 100, 500 };
+	//det.leftTop = { 100, 100 };
+	//det.rightBottom = { 500, 500 };
+	//det.rightTop = { 500, 100 };
+	//det.center_x = 150;
+	//det.center_y = 150;
+	//rw::rqw::ImagePainter::drawShapesOnSourceImg(image, det, config);
+	//QLabel label;
+	//label.setPixmap(QPixmap::fromImage(image));
+	//label.show();
+
+	LicenseValidation l;
+	l.show();
 
 	return a.exec();
 }
