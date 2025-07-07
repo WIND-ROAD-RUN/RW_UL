7ee3ea8cb634d4e4e5509733ef232eb4d10f64f4
RoadWind <3461571786@qq.com>
2025-07-07 11:20:53 +0800

    重构cmake脚本命名

--- cmake/PackageFind.cmake
+++ cmake/PackageFind.cmake
@@ -1,4 +1,4 @@
-# Halcon
+# Halcon | Include: Halcon_include | Lib: Halcon_libs | 
 if(WIN32)
     if(INSTALL_HALCON)
 		find_all_libs(${HALCON_ROOT}/lib/x64-win64 Halcon_libs)
@@ -7,20 +7,20 @@ if(WIN32)
 else()
 endif()
 
-# TensorRT
+# TensorRT | Include: TensorRT_include | Lib: TensorRT_libs | Dll: TensorRT_dlls |Exe: TensorRT_exes |
 set(TensorRTPath ${CMAKE_CURRENT_SOURCE_DIR}/ThirdLibrary/TensorRT-10.9.0.34)
 set(TensorRT_include ${TensorRTPath}/include)
 find_all_libs(${TensorRTPath}/lib TensorRT_libs)
 find_all_dlls(${TensorRTPath}/lib TensorRT_dlls)
 find_all_exes(${TensorRTPath}/bin TensorRT_exes)
 
-# OnnxRuntime
+# OnnxRuntime | Include: OnnxRuntime_include | Lib: OnnxRuntime_libs | Dll: OnnxRuntime_dlls | 
 set(OnnxRuntimePath ${CMAKE_CURRENT_SOURCE_DIR}/ThirdLibrary/onnxruntime-win-x64-gpu-1.21.0)
 set(OnnxRuntime_include ${OnnxRuntimePath}/include)
 find_all_libs(${OnnxRuntimePath}/lib OnnxRuntime_libs)
 find_all_dlls(${OnnxRuntimePath}/lib OnnxRuntime_dlls)
 
-# ZMotion
+# ZMotion | Include: ZMotionRutime_include | Lib: ZMotionRutime_libs | Dll: ZMotionRutime_dlls | 
 set(ZMotionRutimePath ${CMAKE_CURRENT_SOURCE_DIR}/ThirdLibrary/ZMotion)
 set(ZMotionRutime_include ${ZMotionRutimePath}/include)
 find_all_libs(${ZMotionRutimePath}/lib ZMotionRutime_libs)
