d65f188cb7213dd6d55924cb2419d61ac2fbd86e
RoadWind <3461571786@qq.com>
2025-07-07 11:28:24 +0800

    完成halocn脚本配置

--- RW_UL/halcon/test/include/halcon_pch_t.hpp
+++ RW_UL/halcon/test/include/halcon_pch_t.hpp
@@ -1,1 +1,5 @@
+#pragma once
+
 #include <gtest/gtest.h>
+
+#include"Halcon.h"
--- RW_UL/halcon/test/src/test.cpp
+++ RW_UL/halcon/test/src/test.cpp
@@ -1,9 +1,45 @@
+#include "halcon_pch_t.hpp"
+#include"halconcpp/HalconCpp.h"
 
+using namespace HalconCpp;
 
-#include"halcon_pch_t.hpp"
-
-TEST(a,b)
+TEST(HalconExample, DetectCircles)
 {
-	SUCCEED();
+    try
+    {
+        // 初始化 Halcon 窗口
+        HObject ho_Image, ho_Regions, ho_Circles;
+        HTuple hv_WindowID;
 
+        // 加载图像
+        ReadImage(&ho_Image, "fabrik");
+
+        // 创建窗口以显示图像
+        SetWindowAttr("background_color", "black");
+        OpenWindow(0, 0, 512, 512, 0, "visible", "", &hv_WindowID);
+        HDevWindowStack::Push(hv_WindowID);
+
+        // 显示图像
+        if (HDevWindowStack::IsOpen())
+            DispObj(ho_Image, HDevWindowStack::GetActive());
+
+        // 检测圆形对象
+        Threshold(ho_Image, &ho_Regions, 128, 255);
+        Connection(ho_Regions, &ho_Regions);
+        SelectShape(ho_Regions, &ho_Circles, "circularity", "and", 0.8, 1.0);
+
+        // 显示检测到的圆
+        if (HDevWindowStack::IsOpen())
+            DispObj(ho_Circles, HDevWindowStack::GetActive());
+
+        // 关闭窗口
+        CloseWindow(hv_WindowID);
+
+        SUCCEED();
+    }
+    catch (HException& e)
+    {
+        std::cerr << "Halcon Error: " << e.ErrorMessage() << std::endl;
+        FAIL();
+    }
 }
