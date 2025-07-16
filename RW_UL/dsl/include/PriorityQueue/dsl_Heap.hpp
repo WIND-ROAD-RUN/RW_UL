7f0852d00dc7852d5f24d2966a5c5c6103ef18a6
RoadWind <3461571786@qq.com>
2025-07-16 16:59:50 +0800

    重构命名

--- RW_UL/dsl/include/PriorityQueue/dsl_Heap.hpp
+++ RW_UL/dsl/include/PriorityQueue/dsl_Heap.hpp
@@ -17,56 +17,56 @@ namespace rw {
         public:
             // 构造函数，允许预设容量
             explicit ThreadSafeHeap(size_t initialCapacity = 50) {
-                heap_.reserve(initialCapacity);
+                _heap_.reserve(initialCapacity);
             }
 
             // 插入元素
             void push(T value) {
-                std::unique_lock<std::shared_mutex> lock(mutex_);
-                heap_.push_back(value);
+                std::unique_lock<std::shared_mutex> lock(_mutex_);
+                _heap_.push_back(value);
                 if constexpr (Type == HeapType::MinHeap) {
-                    std::push_heap(heap_.begin(), heap_.end(), std::greater<>()); // 最小堆
+                    std::push_heap(_heap_.begin(), _heap_.end(), std::greater<>()); // 最小堆
                 }
                 else {
-                    std::push_heap(heap_.begin(), heap_.end(), std::less<>()); // 最大堆
+                    std::push_heap(_heap_.begin(), _heap_.end(), std::less<>()); // 最大堆
                 }
             }
 
             // 获取堆顶值（无锁读取）
             bool tryGetTop(T& topValue) {
-                std::shared_lock<std::shared_mutex> lock(mutex_);
-                if (heap_.empty()) {
+                std::shared_lock<std::shared_mutex> lock(_mutex_);
+                if (_heap_.empty()) {
                     return false; // 堆为空
                 }
-                topValue = heap_.front(); // 堆顶即为最值
+                topValue = _heap_.front(); // 堆顶即为最值
                 return true;
             }
 
             // 删除堆顶值
             bool tryPopTop(T& topValue) {
-                std::unique_lock<std::shared_mutex> lock(mutex_);
-                if (heap_.empty()) {
+                std::unique_lock<std::shared_mutex> lock(_mutex_);
+                if (_heap_.empty()) {
                     return false; // 堆为空
                 }
-                topValue = heap_.front(); // 堆顶即为最值
+                topValue = _heap_.front(); // 堆顶即为最值
                 if constexpr (Type == HeapType::MinHeap) {
-                    std::pop_heap(heap_.begin(), heap_.end(), std::greater<>()); // 最小堆
+                    std::pop_heap(_heap_.begin(), _heap_.end(), std::greater<>()); // 最小堆
                 }
                 else {
-                    std::pop_heap(heap_.begin(), heap_.end(), std::less<>()); // 最大堆
+                    std::pop_heap(_heap_.begin(), _heap_.end(), std::less<>()); // 最大堆
                 }
-                heap_.pop_back(); // 删除末尾元素
+                _heap_.pop_back(); // 删除末尾元素
                 return true;
             }
 
             size_t size() const {
-                std::shared_lock<std::shared_mutex> lock(mutex_);
-                return heap_.size();
+                std::shared_lock<std::shared_mutex> lock(_mutex_);
+                return _heap_.size();
             }
 
         private:
-            std::vector<T> heap_; 
-            mutable std::shared_mutex mutex_; 
+            std::vector<T> _heap_; 
+            mutable std::shared_mutex _mutex_; 
         };
     }
 
