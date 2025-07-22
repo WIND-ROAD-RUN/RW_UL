9e8fe916943ead7ace03372372b4556954a24f63
RoadWind <3461571786@qq.com>
2025-07-22 15:25:37 +0800

    补充测试代码

--- RW_UL/Module/ImgPro/test/include/imgPro_ImageProcess_t.hpp
+++ RW_UL/Module/ImgPro/test/include/imgPro_ImageProcess_t.hpp
@@ -133,7 +133,7 @@ protected:
 		drawConfig.isDrawDisableDefects = true;
 		drawConfig.setAllIdsWithSameColor({ 0,1,2,3,4,5,6 }, rw::rqw::RQWColor::Brown, true);
 		drawConfig.setAllIdsWithSameColor({ 0,1,2,3,4,5,6 }, rw::rqw::RQWColor::Blue, false);
-
+		drawConfig.classIdWithColorWhichIsGood[2] = rw::rqw::RQWColor::Green;
 		context.defectDrawCfg = drawConfig;
 
 		context.runTextConfig.isDrawExtraText = true;
