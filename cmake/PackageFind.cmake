163e22dc818d723841f8d0b7af757d004b5d4b90
RoadWind <3461571786@qq.com>
2025-08-07 13:27:44 +0800

    添加ZFKJ_DEVELOPMENT_ROOT环境变量查询逻辑

--- cmake/PackageFind.cmake
+++ cmake/PackageFind.cmake
@@ -15,13 +15,13 @@ find_all_dlls(${TensorRTPath}/lib TensorRT_dlls)
 find_all_exes(${TensorRTPath}/bin TensorRT_exes)
 
 # OnnxRuntime | Include: OnnxRuntime_include | Lib: OnnxRuntime_libs | Dll: OnnxRuntime_dlls | 
-set(OnnxRuntimePath ${CMAKE_CURRENT_SOURCE_DIR}/ThirdLibrary/onnxruntime-win-x64-gpu-1.21.0)
+set(OnnxRuntimePath ${ZFKJ_DEVELOPMENT_ROOT}/ThirdLibrary/onnxruntime-win-x64-gpu-1.21.0)
 set(OnnxRuntime_include ${OnnxRuntimePath}/include)
 find_all_libs(${OnnxRuntimePath}/lib OnnxRuntime_libs)
 find_all_dlls(${OnnxRuntimePath}/lib OnnxRuntime_dlls)
 
 # ZMotion | Include: ZMotionRutime_include | Lib: ZMotionRutime_libs | Dll: ZMotionRutime_dlls | 
-set(ZMotionRutimePath ${CMAKE_CURRENT_SOURCE_DIR}/ThirdLibrary/ZMotion)
+set(ZMotionRutimePath ${ZFKJ_DEVELOPMENT_ROOT}/ThirdLibrary/ZMotion)
 set(ZMotionRutime_include ${ZMotionRutimePath}/include)
 find_all_libs(${ZMotionRutimePath}/lib ZMotionRutime_libs)
 find_all_dlls(${ZMotionRutimePath}/dll ZMotionRutime_dlls)
