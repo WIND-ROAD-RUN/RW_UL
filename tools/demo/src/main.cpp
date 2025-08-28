ef1f39b0118016eb0bca029d3763c4ea1089848c
RoadWind <3461571786@qq.com>
2025-08-28 15:55:37 +0800

    完成画框的bug问题

--- tools/demo/src/main.cpp
+++ tools/demo/src/main.cpp
@@ -4,12 +4,13 @@
 #include <QtWidgets/QApplication>
 #include"DlgCloseForm.h"
 #include "rqw_ImageSaveEngineV1.hpp"
+#include"PicturesPainterVersionDunDai.h"
 
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
 
-	rw::rqw::ImageSaveEngineV1 engine;
+	/*rw::rqw::ImageSaveEngineV1 engine;
 	engine.startEngine();
 
 	for (int i=0;i<100000;i++)
@@ -21,7 +22,20 @@ int main(int argc, char* argv[])
 		engine.pushImage(info);
 	}
 
-	engine.stopEngine();
+	engine.stopEngine();*/
+
+	PicturesPainterVersionDunDai painter;
+	QImage image(R"(C:\Users\rw\Desktop\temp\2025-06-17_11-10-55-662.jpg)");
+	//QImage image(R"(C:\Users\rw\Desktop\temp\total.png)");
+	std::vector<rw::rqw::RectangeConfig> cfgs;
+	rw::rqw::RectangeConfig config;
+	config.classid = 0;
+	config.color = QColor(255, 0, 0);
+	config.name = "name_0";
+	cfgs.push_back(config);
+	painter.setRectangleConfigs(cfgs);
+	painter.setImage(image);
+	painter.show();
 
 	return a.exec();
 }
