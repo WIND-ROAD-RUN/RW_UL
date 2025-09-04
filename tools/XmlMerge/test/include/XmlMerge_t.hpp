ac65dc3ed30c9175a9679d23791002161e370fd6
zfkj4090 <zfkj4090@DESKTOP-GJ77VA6>
2025-09-04 13:48:59 +0800

    添加XmlMerge

---
+++ tools/XmlMerge/test/include/XmlMerge_t.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include"XmlMerge.hpp"
+#include"gtest/gtest.h"
---
+++ tools/XmlMerge/test/src/XmlMerge_t.cpp
@@ -0,0 +1,14 @@
+#include"XmlMerge_t.hpp"
+
+
+namespace XmlMerge
+{
+	TEST(a,b)
+	{
+
+
+		ASSERT_EQ(1, 1);
+	}
+
+
+}
