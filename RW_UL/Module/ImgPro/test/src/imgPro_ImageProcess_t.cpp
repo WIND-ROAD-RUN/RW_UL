0d436d5f447bbe6befae37eec4daca89a0432974
RoadWind <3461571786@qq.com>
2025-09-16 09:13:12 +0800

    添加ImageProcessMutiThread测试

--- RW_UL/Module/ImgPro/test/src/imgPro_ImageProcess_t.cpp
+++ RW_UL/Module/ImgPro/test/src/imgPro_ImageProcess_t.cpp
@@ -4,24 +4,52 @@
 #include "ime_utilty.hpp"
 
 
-TEST_F(ImageProcessTest, ImageProcess)
+//TEST_F(ImageProcessTest, ImageProcess)
+//{
+//	int argc = 0;
+//	char* argv[] = { nullptr };
+//	QApplication app(argc, argv);
+//
+//	cv::Mat image = cv::imread(R"(C:\Users\rw\Desktop\temp\niukou.png)");
+//
+//
+//	for (int i=0;i<50;i++)
+//	{
+//		(*imgProcess)(image);
+//	}
+//	auto maskImg = imgProcess->getMaskImg(image);
+//
+//	QLabel label;
+//	label.setPixmap(QPixmap::fromImage(maskImg));
+//	label.show();
+//
+//	app.exec();
+//}
+
+
+void func(int id) {
+	std::cout << "id: " << id << " is running\n";
+}
+
+TEST_F(ImageProcessTest, ImageProcessMutiThread)
 {
-	int argc = 0;
-	char* argv[] = { nullptr };
-	QApplication app(argc, argv);
+    int argc = 0;
+    char* argv[] = { nullptr };
+    QApplication app(argc, argv);
 
-	cv::Mat image = cv::imread(R"(C:\Users\rw\Desktop\temp\niukou.png)");
+    cv::Mat image = cv::imread(R"(C:\Users\rw\Desktop\temp\niukou.png)");
 
+    const int threadCount = 8;
+    std::vector<std::thread> threads;
 
-	for (int i=0;i<50;i++)
-	{
-		(*imgProcess)(image);
-	}
-	auto maskImg = imgProcess->getMaskImg(image);
+    for (int i = 0; i < threadCount; ++i) {
+        threads.emplace_back([this, &image]() {
+            (*imgProcess)(image);
+            std::cout << imgProcess->getProcessResult().size() + "\n";
+            });
+    }
 
-	QLabel label;
-	label.setPixmap(QPixmap::fromImage(maskImg));
-	label.show();
+    for (auto& t : threads) t.join();
 
-	app.exec();
+    app.exit();
 }
